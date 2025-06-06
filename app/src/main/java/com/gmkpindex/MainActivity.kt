6108deb01fcb4f07708a05eafd9d3a79f92d5d41
Vladimir Vaganov <none@none.com>
2025-06-06 18:35:29 +0300

    refactor: main activity de-composite MainActivity.sendGet on specific purpose methods

--- app/src/main/java/com/gmkpindex/MainActivity.kt
+++ app/src/main/java/com/gmkpindex/MainActivity.kt
@@ -15,6 +15,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.URL
 import java.net.HttpURLConnection
+import kotlin.collections.component1
+import kotlin.collections.component2
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
@@ -41,6 +43,7 @@ val impactMap: Map<ImpactEnum, ImpactMeta> = mapOf(
 
 class MainActivity : ComponentActivity() {
 
+    var rawData = ""
     var data = ""
 
     @OptIn(ExperimentalTime::class)
@@ -50,19 +53,16 @@ class MainActivity : ComponentActivity() {
     var timestamp = timeSource.markNow()
     override fun onStart() {
         super.onStart()
-        val textView = this.findViewById<TextView>(R.id.dataField)
-        rewriteData(textView)
-        textView.text = this.data
+        rewriteData()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         val textView = this.findViewById<TextView>(R.id.dataField)
-
         val buttonView = this.findViewById<Button>(R.id.button)
         buttonView.setOnClickListener {
-            rewriteData(textView)
+            refreshData(textView)
         }
         val buttonInfoView = this.findViewById<Button>(R.id.buttonInfo)
         buttonInfoView.setOnClickListener {
@@ -72,141 +72,147 @@ class MainActivity : ComponentActivity() {
         infoScrollView.visibility = View.INVISIBLE
     }
 
-    private fun sendGet(view: TextView) {
-        val url = URL(resources.getString(R.string.dataURL))
-        val noResStr = resources.getString(R.string.noRes)
+    private fun getImpactMeta(geomagneticIdx: Double): ImpactMeta? {
+        return if (geomagneticIdx < impactMap[ImpactEnum.MINOR]!!.value) {
+                impactMap[ImpactEnum.CALM]
+            } else if (impactMap[ImpactEnum.MINOR]!!.value <= geomagneticIdx &&
+                geomagneticIdx < impactMap[ImpactEnum.MODERATE]!!.value
+            ) {
+                impactMap[ImpactEnum.MINOR]
+            } else if (impactMap[ImpactEnum.MODERATE]!!.value <= geomagneticIdx &&
+                geomagneticIdx < impactMap[ImpactEnum.STRONG]!!.value
+            ) {
+                impactMap[ImpactEnum.MODERATE]
+            } else if (impactMap[ImpactEnum.STRONG]!!.value <= geomagneticIdx &&
+                geomagneticIdx < impactMap[ImpactEnum.SEVERE]!!.value
+            ) {
+                impactMap[ImpactEnum.STRONG]
+            }else if (impactMap[ImpactEnum.SEVERE]!!.value <= geomagneticIdx &&
+                geomagneticIdx < impactMap[ImpactEnum.EXTREME]!!.value
+            ) {
+                impactMap[ImpactEnum.SEVERE]
+            }
+            else if  (impactMap[ImpactEnum.EXTREME]!!.value <= geomagneticIdx){
+                impactMap[ImpactEnum.EXTREME]
+            } else {
+                impactMap[ImpactEnum.CALM]
+            }
+    }
+
+    private fun parseData(dataStr: String): Pair<MutableList<String>, Double> {
         var last2Dates: MutableList<List<String>>
         var last2DatesData: MutableList<String> = mutableListOf()
-        var mainView = this.findViewById<View>(R.id.activityMain)
-        var curIdxView = this.findViewById<TextView>(R.id.curIdx)
-        var curIdxNameView = this.findViewById<TextView>(R.id.curIdxName)
-        var curIdxInfoView = this.findViewById<TextView>(R.id.curIdxInfo)
-        val appContext = this.applicationContext
+        val pattern = resources.getString(R.string.regexpPattern).toRegex()
+        val dates: List<String> =dataStr.split("\n").dropLast(1).toList()
+
+        last2Dates = dates.subList(dates.size - 2, dates.size).map{
+                d -> pattern.find(d)?.groupValues?.drop(1)
+        } as MutableList<List<String>>
+        val (yesterdayData, todayData) = last2Dates
+
+        // previous day info
+        last2DatesData.add(resources.getString(R.string.dateStr) + ": \n" + yesterdayData[0])
+        last2DatesData.add(resources.getString(R.string.midStr) + ": \n" + yesterdayData[1])
+        last2DatesData.add(resources.getString(R.string.highStr) + ": \n" + yesterdayData[2])
+        val prevDateIdxs: String = yesterdayData[3].toString()
+        last2DatesData.add(resources.getString(R.string.estStr) + ": \n" + yesterdayData[3] + "\n")
+
+        // current day info
+        last2DatesData.add(resources.getString(R.string.dateStr) + ": \n" + todayData[0])
+        last2DatesData.add(resources.getString(R.string.midStr) + ": \n" + todayData[1])
+        last2DatesData.add(resources.getString(R.string.highStr) + ": \n" + todayData[2])
+
+        var rippedLastDateIdx: Double = -1.0
+
+        for (idx in todayData[3].toString()
+            .split(" ").asReversed().dropLast(1)) {
+            if (
+                idx != "-1.00" &&
+                idx.contains(".")
+            ) {
+                rippedLastDateIdx = idx.toDouble()
+                break
+            }
+        }
+        if (rippedLastDateIdx == -1.0) {
+            for (idx in prevDateIdxs.split(" ")
+                .asReversed()
+                .dropLast(1)) {
+                if (
+                    idx != "-1.00" &&
+                    idx.contains(".")
+                ) {
+                    rippedLastDateIdx = idx.toDouble()
+                    break
+                }
+            }
+        }
+        last2DatesData.add(this.applicationContext.getString(R.string.estStr) + ": \n" + todayData[3])
+        return Pair(last2DatesData, rippedLastDateIdx)
+    }
+
+
+    private fun rewriteData(
+        view: TextView = this@MainActivity.findViewById<TextView>(R.id.dataField)
+    ) {
+        val url = URL(resources.getString(R.string.dataURL))
         lifecycleScope.launch {
             withContext(Dispatchers.IO) {
                 try {
                     with(url.openConnection() as HttpURLConnection) {
                         requestMethod = "GET"
-                        connectTimeout = 10000
-                        readTimeout = 10000
+                        connectTimeout = 1000
+                        readTimeout = 1000
                         if (responseCode in 200..299) {
-                            val pattern = resources.getString(R.string.regexpPattern).toRegex()
-
-                            val dates: List<String> =
-                                url.readText().split("\n").dropLast(1).toList()
-
-                            last2Dates = dates.subList(dates.size - 2, dates.size).map{
-                                    d -> pattern.find(d)?.groupValues?.drop(1)
-                                } as MutableList<List<String>>
-                            val (yesterdayData, todayData) = last2Dates
-
-                            // previous day info
-                            last2DatesData.add(appContext.getString(R.string.dateStr) + ": \n" + yesterdayData[0])
-                            last2DatesData.add(appContext.getString(R.string.midStr) + ": \n" + yesterdayData[1])
-                            last2DatesData.add(appContext.getString(R.string.highStr) + ": \n" + yesterdayData[2])
-                            val prevDateIdxs: String = yesterdayData[3].toString()
-                            last2DatesData.add(appContext.getString(R.string.estStr) + ": \n" + yesterdayData[3] + "\n")
-
-                            // current day info
-                            last2DatesData.add(appContext.getString(R.string.dateStr) + ": \n" + todayData[0])
-                            last2DatesData.add(appContext.getString(R.string.midStr) + ": \n" + todayData[1])
-                            last2DatesData.add(appContext.getString(R.string.highStr) + ": \n" + todayData[2])
-
-                            var rippedLastDateIdx: Double = -1.0
-
-                            for (idx in todayData[3].toString()
-                                .split(" ").asReversed().dropLast(1)) {
-                                if (
-                                    idx != "-1.00" &&
-                                    idx.contains(".")
-                                ) {
-                                    rippedLastDateIdx = idx.toDouble()
-                                    break
-                                }
-                            }
-                            if (rippedLastDateIdx == -1.0) {
-                                for (idx in prevDateIdxs.split(" ")
-                                    .asReversed()
-                                    .dropLast(1)) {
-                                    if (
-                                        idx != "-1.00" &&
-                                        idx.contains(".")
-                                    ) {
-                                        rippedLastDateIdx = idx.toDouble()
-                                        break
-                                    }
-                                }
-                            }
-                            last2DatesData.add(appContext.getString(R.string.estStr) + ": \n" + todayData[3])
-
-                            var impactScl: ImpactMeta? = impactMap[ImpactEnum.MINOR]
-                            impactScl =
-                                if (rippedLastDateIdx < impactMap[ImpactEnum.MINOR]!!.value) {
-                                    impactMap[ImpactEnum.CALM]
-                                } else if (impactMap[ImpactEnum.MINOR]!!.value <= rippedLastDateIdx &&
-                                    rippedLastDateIdx < impactMap[ImpactEnum.MODERATE]!!.value
-                                ) {
-                                impactMap[ImpactEnum.MINOR]
-                                } else if (impactMap[ImpactEnum.MODERATE]!!.value <= rippedLastDateIdx &&
-                                    rippedLastDateIdx < impactMap[ImpactEnum.STRONG]!!.value
-                                ) {
-                                    impactMap[ImpactEnum.MODERATE]
-                                } else if (impactMap[ImpactEnum.STRONG]!!.value <= rippedLastDateIdx &&
-                                    rippedLastDateIdx < impactMap[ImpactEnum.SEVERE]!!.value
-                                ) {
-                                    impactMap[ImpactEnum.STRONG]
-                                }else if (impactMap[ImpactEnum.SEVERE]!!.value <= rippedLastDateIdx &&
-                                    rippedLastDateIdx < impactMap[ImpactEnum.EXTREME]!!.value
-                                ) {
-                                    impactMap[ImpactEnum.SEVERE]
-                                }
-                                else if  (impactMap[ImpactEnum.EXTREME]!!.value <= rippedLastDateIdx){
-                                    impactMap[ImpactEnum.EXTREME]
-                                } else {
-                                    impactMap[ImpactEnum.CALM]
-                                }
-
+                            rawData = url.readText()
                             withContext(Dispatchers.Main) {
-                                val dataStr = last2DatesData.joinToString("\n")
-                                if (!dataStr.isEmpty()) {
-                                    view.text = dataStr
-                                    curIdxView.text = rippedLastDateIdx.toString()
-                                    curIdxView.setTextColor(appContext.getColor(impactScl!!.color))
-                                    view.setBackgroundColor(appContext.getColor(impactScl.color))
-                                    mainView.setBackgroundColor(appContext.getColor(impactScl.color))
-                                    curIdxNameView.text = impactScl.lvl
-                                    curIdxInfoView.text = HtmlCompat.fromHtml(
-                                        appContext.getString(impactScl.desc),
-                                        HtmlCompat.FROM_HTML_MODE_LEGACY
-                                    )
-                                } else {
-                                    view.text = noResStr
-                                }
-                                data = view.text.toString()
+                                updateViews(view)
                             }
-                        } else {
-                            Log.e("HTTP ERROR", url.readText().toString())
-                            view.text = noResStr
+                        }else{
+                            Log.e("HTTP_ERROR", responseCode.toString())
                         }
                     }
-
                 } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Log.e("Error:", e.stackTraceToString())
-                        view.text = noResStr
-                    }
+                    Log.e("Error:", e.stackTraceToString())
                 }
             }
+
+        }
+    }
+
+    private fun updateViews(view: TextView){
+        val noResStr = resources.getString(R.string.noRes)
+        var mainView = this.findViewById<View>(R.id.activityMain)
+        var curIdxView = this.findViewById<TextView>(R.id.curIdx)
+        var curIdxNameView = this.findViewById<TextView>(R.id.curIdxName)
+        var curIdxInfoView = this.findViewById<TextView>(R.id.curIdxInfo)
+        if (!this.rawData.isEmpty()) {
+            val(last2DatesData, rippedLastDateIdx) =
+                parseData(this.rawData)
+            var impactScl: ImpactMeta? = getImpactMeta(rippedLastDateIdx.toDouble())
+            this.data = last2DatesData.joinToString("\n")
+            view.text = this.data
+            curIdxView.text = rippedLastDateIdx.toString()
+            curIdxView.setTextColor(resources.getColor(impactScl!!.color))
+            view.setBackgroundColor(resources.getColor(impactScl.color))
+            mainView.setBackgroundColor(resources.getColor(impactScl.color))
+            curIdxNameView.text = impactScl.lvl
+            curIdxInfoView.text = HtmlCompat.fromHtml(
+                resources.getString(impactScl.desc),
+                HtmlCompat.FROM_HTML_MODE_LEGACY
+            )
+        } else {
+            view.text = noResStr
         }
     }
 
     @OptIn(ExperimentalTime::class)
-    fun rewriteData(view: TextView) {
+    fun refreshData(view: TextView) {
         val timeDiff = this.timeSource.markNow() - this.timestamp
-        if (timeDiff.inWholeSeconds >= resources.getInteger(R.integer.refreshDelay) || this.data.isEmpty()) {
+        if (timeDiff.inWholeSeconds >= resources.getInteger(R.integer.refreshDelay) || this.rawData.isEmpty()) {
             this.timestamp = this.timeSource.markNow()
             Log.i("TIME", timeDiff.toString())
-            sendGet(view)
+            rewriteData(view)
         }
     }
 
